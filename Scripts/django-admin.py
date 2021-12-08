aaa2aae06195356c1984ddfec1e0e2376be33c67
Georgi Tanev <georgi.tanev86@gmail.com>
2021-12-08 17:09:19 +0200

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\git\24_09_2019_download_repos\byob-profiles-rest-api-master\byob-profiles-rest-api-master\deploy_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
